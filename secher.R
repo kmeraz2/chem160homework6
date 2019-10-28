75a80fabbe38bb37c5711f08e40e03b1068a861e
kmeraz2 <54649455+kmeraz2@users.noreply.github.com>
2019-10-27 22:29:43 -0700

    Add files via upload

---
+++ secher.R
@@ -0,0 +1,16 @@
+secher<-read.table("secher.txt",header=T)
+model<-lm(bwt~bpd, data=secher) 
+sum.model<-summary(model)
+R2<-sum.model$r.squared
+f<-sum.model$fstatistic 
+p.value<-pf(f[1],f[2],f[3],lower.tail=F)
+output<-sprintf("R2 = %f and p-value=%f", R2, p.value)
+cat(output)
+intercept<-model$coefficients[1] 
+slope<-model$coefficients[2]
+output<-sprintf("slope=%f  intercept=%f",slope, intercept)
+cat(output)
+png("graph.png")
+plot(bwt~bpd, data=secher) 
+abline(model)
+dev.off()
